ec45735cf93c06e7b368c0d13cfa250d23884300
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-02-14 04:00:20 +0530

    Update package.json and theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: <span> PUBG PC Clone</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/iamutkarshyadav/documentation1',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/iamutkarshyadav/documentation1',
   footer: {
     text: 'PUBG PC Clone Project | All rights reserved © Utkarsh Yadav',
   },
-}
+};
 
-export default config
+export default config;
