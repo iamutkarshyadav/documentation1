bb9e2dd820f323ac8cad3d1a38cd9250ffb41b80
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-02-13 06:47:23 +0530

    Update _meta.json and theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>&copy; PUBG PC Clone</span>,
+  logo: <span> PUBG PC Clone</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
